28bb969735aa04e79b3fe285844a7579d102a5c3
Bilal <2023513602.bilal@ug.sharda.ac.in>
2024-08-22 01:10:39 +0530

    Initial commit

---
+++ Documents/main.java
@@ -0,0 +1,8 @@
+main.java
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
---
+++ Documents/math_operations_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def add(x, y):
+    return x + y
+
+
+# In[5]:
+
+
+def subtract(x, y):
+    return x - y
+
